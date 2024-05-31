54316f5f5f4cf4d9793b20f1c81fb27bc8dbc0cf
kolobo4ok <142622191+kolobo4ok@users.noreply.github.com>
2024-05-31 04:02:34 +0300

    CreatorShortURL - контролер, що обробляє запити на створення коротких URL і видає інформацію у вигляді json

--- src/main/java/com/example/URL_Shortener/entity/NewShortURL.java
+++ src/main/java/com/example/URL_Shortener/entity/NewShortURL.java
@@ -1,16 +1,26 @@
 package com.example.URL_Shortener.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.LocalDate;
 
+
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class NewShortURL {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Ідентифікатор сутності
 
     @NotNull
     private String originURL;
@@ -25,4 +35,12 @@ public class NewShortURL {
     private LocalDate creatingDate;
 
     private LocalDate finishingDate;
+
+    // не знаю у мене чомусь без конструктора не пашить
+    public NewShortURL(String originURL, String shortURL, LocalDate creatingDate, LocalDate finishingDate) {
+        this.originURL = originURL;
+        this.shortURL = shortURL;
+        this.creatingDate = creatingDate;
+        this.finishingDate = finishingDate;
+    }
 }
