debc24bb3e0a09b2a3f3620592ecf09d26bd543f
BBohdann <bbodnar2003@gmail.com>
2024-06-02 21:59:49 +0300

    Created URL validator class and tests

---
+++ src/main/java/com/example/URL_Shortener/service/UrlValidator.java
@@ -0,0 +1,70 @@
+package com.example.URL_Shortener.service;
+
+import com.example.URL_Shortener.service.exceptions.InvalidUrlException;
+import org.springframework.stereotype.Component;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+/**
+ * Клас для перевірки валідності URL-адреси.
+ */
+@Component
+public class UrlValidator {
+    private static final String URL_PATTERN = "^(http|https)://.*$";
+    private static final Pattern pattern = Pattern.compile(URL_PATTERN);
+
+    /**
+     * Перевіряє, чи є URL-адреса валідною.
+     *
+     * @param urlString URL-адреса для перевірки.
+     * @return true, якщо URL-адреса є валідною, в іншому випадку викидає виключення InvalidUrlException.
+     * @throws InvalidUrlException викидається, якщо URL-адреса є невалідною.
+     */
+    public boolean isValidUrl(String urlString) throws InvalidUrlException {
+        if (pattern.matcher(urlString).matches() && isValidConnection(urlString))
+            return true;
+                else
+                    throw new InvalidUrlException("Invalid URL: " + urlString);
+    }
+
+    /**
+     * Перевіряє, чи можна підключитися до URL-адреси.
+     *
+     * @param urlString URL-адреса для перевірки.
+     * @return true, якщо можна підключитися до URL-адреси та статус-код є валідним, в іншому випадку викидає виключення InvalidUrlException.
+     * @throws InvalidUrlException викидається, якщо не можна підключитися до URL-адреси або отриманий недійсний статус-код.
+     */
+    private boolean isValidConnection(String urlString) throws InvalidUrlException {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            if (connection != null) {
+                int statusCode = connection.getResponseCode();
+                return isValidStatusCode(statusCode);
+            } else {
+                throw new InvalidUrlException("Failed to connect on this URL: " + urlString);
+            }
+        } catch (Exception e) {
+            throw new InvalidUrlException("Failed to validate URL: " + urlString);
+        } finally {
+            if (connection != null)
+                connection.disconnect();
+        }
+    }
+
+    /**
+     * Перевіряє, чи є статус-код валідним.
+     *
+     * @param statusCode статус-код для перевірки.
+     * @return true, якщо статус-код є валідним, в іншому випадку false.
+     */
+    private boolean isValidStatusCode(int statusCode) {
+        return statusCode == HttpURLConnection.HTTP_FORBIDDEN ||
+                statusCode == HttpURLConnection.HTTP_UNAUTHORIZED ||
+                statusCode < HttpURLConnection.HTTP_BAD_REQUEST ||
+                statusCode >= HttpURLConnection.HTTP_INTERNAL_ERROR;
+    }
+}
---
+++ src/test/java/com/example/URL_Shortener/UrlValidatorTest.java
@@ -0,0 +1,50 @@
+package com.example.URL_Shortener;
+
+import com.example.URL_Shortener.service.exceptions.InvalidUrlException;
+import com.example.URL_Shortener.service.UrlValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class UrlValidatorTest {
+    private UrlValidator urlValidator;
+
+    @BeforeEach
+    public void setUp() {
+        urlValidator = new UrlValidator();
+    }
+
+    @Test
+    void testValidUrl() throws InvalidUrlException {
+        //Перевірка валідатора на статус код 200(ОК)
+        Assertions.assertEquals(true, urlValidator.isValidUrl("https://blogs.nasa.gov/boeing-crew-flight-test/"));
+        //Перевірка валідатора на статус код 200(Неавторизовано)
+        Assertions.assertEquals(true, urlValidator.isValidUrl("https://httpstat.us/401"));
+        //Перевірка валідатора на статус код 403(Недостатньо прав)
+        Assertions.assertEquals(true, urlValidator.isValidUrl("https://httpstat.us/403"));
+        //Перевірка валідатора на статус код 300(Перенаправлення)
+        Assertions.assertEquals(true, urlValidator.isValidUrl("http://httpstat.us/300"));
+        //Перевірка валідатора на статус код 500(Помилка сервера)
+        Assertions.assertEquals(true, urlValidator.isValidUrl("https://httpstat.us/500"));
+    }
+    @Test
+    void testInvalidUrl() throws InvalidUrlException {
+        //Перевірка валідатора на статус код 404(Не знайдено)
+        Assertions.assertEquals(false, urlValidator.isValidUrl("https://httpstat.us/404"));
+        //Перевірка валідатора на статус код 420(Надто багато запитів від клієнта)
+        Assertions.assertEquals(false, urlValidator.isValidUrl("https://httpstat.us/420"));
+        //Перевірка валідатора на статус код 450(Заблоковано батьківським контролем)
+        Assertions.assertEquals(false, urlValidator.isValidUrl("https://httpstat.us/450"));
+    }
+    @Test
+    void testInvalidUrlThrowsException() {
+        // Перевірка валідатора на неправильну адресу посилання
+        Assertions.assertThrows(InvalidUrlException.class, () -> {
+            urlValidator.isValidUrl("hps://httpstat.us/404");
+        });
+        Assertions.assertThrows(InvalidUrlException.class, () -> {
+            urlValidator.isValidUrl("fasdaxcasda");
+        });
+    }
+}
