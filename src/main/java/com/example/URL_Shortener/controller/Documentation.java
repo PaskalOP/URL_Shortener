046164b5ae1d663ef7891fdfacd3c3b911fc91cb
AntonBar <BarashASUA@gmail.com>
2024-06-09 14:37:04 +0300

    Відкориговано остновну сторінку . Створено сторінку для презенатацію про нас.

--- src/main/java/com/example/URL_Shortener/controller/Documentation.java
+++ src/main/java/com/example/URL_Shortener/controller/Documentation.java
@@ -1,6 +1,9 @@
 package com.example.URL_Shortener.controller;
 
+import com.example.URL_Shortener.config.SwaggerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,8 +12,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/api/V2/shorter")
 public class Documentation {
 
+    @Autowired
+    private SwaggerProperties swaggerProperties;
+
     @GetMapping("/restapidoc")
-    public ModelAndView getAboutPage() {
-        return new ModelAndView("index");
+    public String getDocumentPage(Model model) {
+        System.out.println("Swagger URL: " + swaggerProperties.getUrl());
+        model.addAttribute("swaggerUrl", swaggerProperties.getUrl());
+        return "index";
     }
 }
