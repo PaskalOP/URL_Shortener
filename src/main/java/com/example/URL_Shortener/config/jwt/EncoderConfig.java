217997ec60592395b43e6241f128717ca3ec6820
BBohdann <bbodnar2003@gmail.com>
2024-06-10 21:43:40 +0300

    Jwt Update

---
+++ src/main/java/com/example/URL_Shortener/config/jwt/EncoderConfig.java
@@ -0,0 +1,25 @@
+package com.example.URL_Shortener.config.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Клас для конфігурації енкодера паролів.
+ * Включає налаштування для використання BCryptPasswordEncoder
+ */
+@Component
+public class EncoderConfig {
+
+    /**
+     * Метод, який повертає бін PasswordEncoder
+     * Використовується для шифрування паролів за допомогою алгоритму BCrypt.
+     *
+     * @return PasswordEncoder - інтерфейс для шифрування паролів
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
